d6dce6ec3f131f371781513fc923404995129e80
gitKatia <katia.iacomussi@libero.it>
2017-11-02 22:21:58 +0000

    Updated Consumer

--- kafka-demo/kafka-basics/src/main/java/com/kat/aio/consumers/KafkaConsumerTest.java
+++ kafka-demo/kafka-basics/src/main/java/com/kat/aio/consumers/KafkaConsumerTest.java
@@ -15,7 +15,7 @@ public class KafkaConsumerTest {
 		properties.put("bootstrap.servers","localhost:9092");
 		properties.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
 		properties.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
-		properties.put("group.id", "test1");
+		properties.put("group.id", "consumers-g1");
 		
 		KafkaConsumer<String,String> consumer = new KafkaConsumer<String,String>(properties);
 		
@@ -28,7 +28,7 @@ public class KafkaConsumerTest {
 				ConsumerRecords<String, String> records = consumer.poll(10);
 				
 				for(ConsumerRecord<String, String> record:records){
-					System.out.println("Record read:" + record.toString());
+					System.out.println("Record:" + record.toString());
 				}
 			}
 		} catch(Exception exc){
