ba8cde6b26d5932dd781621beff8b9dcc90c84af
gitKatia <katia.iacomussi@libero.it>
2017-11-02 22:18:00 +0000

    Added Consumer

---
+++ kafka-demo/kafka-basics/src/main/java/com/kat/aio/consumers/KafkaConsumerTest.java
@@ -0,0 +1,41 @@
+package com.kat.aio.consumers;
+
+import java.util.ArrayList;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class KafkaConsumerTest {
+
+	public static void main(String[] args) {
+
+		Properties properties = new Properties();
+		properties.put("bootstrap.servers","localhost:9092");
+		properties.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+		properties.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+		properties.put("group.id", "test1");
+		
+		KafkaConsumer<String,String> consumer = new KafkaConsumer<String,String>(properties);
+		
+		ArrayList<String> topics = new ArrayList<String>();
+		topics.add("my-first-topic");
+		consumer.subscribe(topics);
+		
+		try {
+			while(true){
+				ConsumerRecords<String, String> records = consumer.poll(10);
+				
+				for(ConsumerRecord<String, String> record:records){
+					System.out.println("Record read:" + record.toString());
+				}
+			}
+		} catch(Exception exc){
+			System.err.println(exc.getMessage());
+		} finally {
+			consumer.close();
+		}
+	}
+
+}
--- kafka-demo/kafka-basics/src/main/java/com/kat/aio/producers/KafkaProducerTest.java
+++ kafka-demo/kafka-basics/src/main/java/com/kat/aio/producers/KafkaProducerTest.java
@@ -14,25 +14,30 @@ public class KafkaProducerTest {
 	public static void main(String[] args) {
 		
 		Properties properties = new Properties();
+		// Broker the Kafka Producer connects to
 		properties.put("bootstrap.servers","localhost:9092");
+		// The Key decides the partition for the Record
+		// Key Serializer used by the Producer
 		properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+		// Payload Serializer used by the Producer
 		properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
 		
-		KafkaProducer<String,String> kProducer = new KafkaProducer<String,String>(properties);
+		KafkaProducer<String,String> kafkaProducer = new KafkaProducer<String,String>(properties);
 		
 		try {
 			
 			for(int k = 0; k< 10; k++){
 				
-				ProducerRecord<String,String> pRecord = new ProducerRecord<String,String>("my-first-topic", "m1", "m1-" + k);
-				kProducer.send(pRecord);
+				ProducerRecord<String,String> producerRecord = 
+						new ProducerRecord<String,String>("my-first-topic", "m1", "m1-" + k);
+				kafkaProducer.send(producerRecord);
 			}
 		}catch(Exception exc){
 			
 			System.out.println(exc.getMessage());
 		} finally {
 			
-			kProducer.close();
+			kafkaProducer.close();
 		}
 	}
 	
