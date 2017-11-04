6a3f00df88dc07ff66446abd940d792420ace554
gitKatia <katia.iacomussi@libero.it>
2017-11-04 10:28:27 +0000

    First Commit for Kafka Admin Console

---
+++ kafka-demo/kafka-web-admin/src/main/java/com/kat/aio/KafkaWebAdminApplication.java
@@ -0,0 +1,12 @@
+package com.kat.aio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWebAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWebAdminApplication.class, args);
+	}
+}
